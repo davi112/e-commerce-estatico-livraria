2a24401ce19fad67cbb39f5bf1c587ad21085b4a
Davi Martins <davimartinsfreitas@gmail.com>
2023-05-26 21:59:26 -0300

    refatora-tudo

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-/*Codigo pode melhorar muito, mas esta razoavelmente funcional dentro do que foi passado na disciplina*/
-
-/*Variavel global, utilizada no metodo de busca*/
-var previous_search_size = 0;
-
 /*Retorna os produtos e constroi o html com os valores*/
 window.onload = () => {
     retrieve_products();
@@ -13,13 +8,8 @@ window.onload = () => {
     window.document.getElementById('btn-search').addEventListener('click', search_items)
 }
 
-/*Retornando todos os produtos e construindo o html com os valores
-.then(function(res){
-    res.json()
-})*/
-
 function retrieve_products() {
-    fetch('http://diwserver.vps.webdock.cloud:8765/products?limit=9')
+    fetch('http://diwserver.vps.webdock.cloud:8765/products')
         .then(res => res.json())
         .then(data => {
             productCards = '';
@@ -42,24 +32,10 @@ function retrieve_products() {
                         <a href="detalhe.html?id=${produto.id}">Descrição</a>
                         <p>$:${produto.price}</p>
 					</div>`
-
-                if (i == 2) {
-                    productCards = set_products_per_row("row1", productCards)
-                } else if (i == 5) {
-                    productCards = set_products_per_row("row2", productCards)
-                } else if (i == 8) {
-                    productCards = set_products_per_row("row3", productCards)
-                }
             }
             document.getElementById('most-viewed-container').innerHTML = mostViewedProducts;
+            document.getElementById('row1').innerHTML = productCards;
         });
-}
-
-/*Define em qual linha do boostrap entrara cada produto*/
-function set_products_per_row(rowId, cards) {
-    document.getElementById(rowId).innerHTML = cards;
-    cards = ''
-    return cards
 }
 
 /*Filtra os produtos pela categoria, desabilitando os botoes quando necessario*/
@@ -73,30 +49,41 @@ function filter_by_category() {
     categoryCombo.disabled = true;
 
     var selectedCategory = categoryCombo.value;
-    var productCards = window.document.getElementsByClassName('card');
+    
+    fetch(`http://diwserver.vps.webdock.cloud:8765/products/category/${selectedCategory}`)
+    .then(res => res.json())
+    .then(data => {
+        productCards = '';
+        mostViewedProducts = '';
+        for (let i = 0; i < data.products.length; i++) {
+            let produto = data.products[i];
 
-    for (let i = 0; i < productCards.length; i++) {
-        var produto = productCards[i];
-        var categoria = produto.getElementsByClassName('category').item(0).textContent;
-
-        if (categoria != selected_category) {
-            produto.style.display = "none";
+            productCards +=
+                `<div class="p-card card col-lg-3 col-md-4 col-xs-12">
+                    <img src=${produto.image} alt="">
+                    <a class="d-block" href="detalhe.html?id=${produto.id}/">Detalhes do Produto</a>
+                    <p class="category">${produto.category}</p>
+                    <p class="book-tag">${produto.title}</p>
+                    <p class="price-tag">R$:${produto.price}</p>
+                </div>`
+            
+            mostViewedProducts += `
+                <div class="destaque">
+                    <img src="${produto.image}" alt="">
+                    <a href="detalhe.html?id=${produto.id}">Descrição</a>
+                    <p>$:${produto.price}</p>
+                </div>`
         }
-    }
+        document.getElementById('most-viewed-container').innerHTML = mostViewedProducts;
+        document.getElementById('row1').innerHTML = productCards;
+    });
 }
 
 /*Remove todos os filtros de categoria*/
 function remove_filter() {
     var product_cards = window.document.getElementsByClassName('card');
     window.document.getElementById('select-category').disabled = false;
-
-    for (let i = 0; i < product_cards.length; i++) {
-        product_cards[i].style.display = 'flex';
-    }
-    alert('Filtro removido');
-    window.document.getElementById('remove-button').style.display = 'none';
-    search_button.disabled = false;
-    search_button.style.opacity = 1;
+    retrieve_products();
 }
 
 /*Retorna todas as categorias e constroi html com os valores*/
@@ -117,21 +104,23 @@ function retrieve_categories() {
 function search_items() {
     var product_cards = window.document.getElementsByClassName('card');
     var input_search = window.document.getElementById('search').value;
-    var current_search_size = input_search.length;
   
-    for (let i = 0; i < product_cards.length; i++) {
-        var produto = product_cards[i];
-        produto.style.display = "flex";
+    fetch(`http://diwserver.vps.webdock.cloud:8765/products/search?query=${input_search}`)
+    .then(res => res.json())
+    .then(data => {
+        productCards = '';
+        for (let i = 0; i < data.length; i++) {
+            let produto = data[i];
 
-        let text_for_search = produto.getElementsByClassName('book-tag').item(0).textContent;
-        text_for_search += produto.getElementsByClassName('price-tag').item(0).textContent;
-        text_for_search = text_for_search.toLowerCase();
-        
-        if(input_search == '' || (current_search_size < previous_search_size && text_for_search.includes(input_search))){
-            produto.style.display = "flex";
-        }else if(!text_for_search.includes(input_search.toLowerCase())){
-            previous_search_size = current_search_size;
-            produto.style.display = "none";
+            productCards +=
+                `<div class="p-card card col-lg-3 col-md-4 col-xs-12">
+                    <img src=${produto.image} alt="">
+                    <a class="d-block" href="detalhe.html?id=${produto.id}/">Detalhes do Produto</a>
+                    <p class="category">${produto.category}</p>
+                    <p class="book-tag">${produto.title}</p>
+                    <p class="price-tag">R$:${produto.price}</p>
+                </div>`
         }
-    } 
+        document.getElementById('row1').innerHTML = productCards;
+    });
 }
